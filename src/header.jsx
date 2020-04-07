9fd14d2e00d17024b1c6d15a43f9462614e169a2
krzysztof-siek <krzysztof.siek01@gmail.com>
2020-04-07 21:01:56 +0200

    initial commit from new react app v2

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <>
+        <h2>welcome from header</h2>
+        <h2>hej</h2>
+        </>
+      );
+}
+
+export default Header;
