1ebb2c9a49fcdb22030f7c7f46f886eecf1321ca
Krzysztof Siek <krzysztof.siek01@gmail.com>
2020-04-18 18:46:00 +0200

    needs to be rebuild

--- src/components/Slider/SliderItem.jsx
+++ src/components/Slider/SliderItem.jsx
@@ -12,16 +12,15 @@ return (
         </div>
         <div className="item-infos">
             <div className="flex-box">
-            <h3 className="item-title">{title}</h3>
-            <h4>{release_date}</h4>
+                <h3 className="item-title">{title}</h3>
+                <h4>{release_date}</h4>
             </div>
             <div className="flex-box">
-            <h4><span className="star">&#9733;</span><span className="star">&#9733;</span><span className="star">&#9733;</span><span className="star">&#9733;</span><span className="star">&#9734;</span></h4>
-            <h4>{vote_average}</h4>
-            <h4>{vote_count}</h4>
+                <h4><span className="star">&#9733;</span><span className="star">&#9733;</span><span className="star">&#9733;</span><span className="star">&#9733;</span><span className="star">&#9734;</span></h4>
+                <h4>{vote_average}</h4>
+                <h4>{vote_count}</h4>
             </div>
             <h4 className="item-desc">{overview}</h4>
-
         </div>
     </div>
 </Link>
